4030b64d2da32925dd662e63d4e8abdd8a5adcde
Dylan Greene <dylantgreene@outlook.com>
2018-12-09 15:57:24 -0500

    now using Ranked Measure Data sheet, thus updated script to generate that sheets csv files to fix header

--- xls_to_csv.sh
+++ xls_to_csv.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+### Perform xls to csv conversion
+
+# Each xls has 7 sheets, each sheet will generate a separate csv numbered 0-6
 for file in data/xls/*.xls; do
     base=${file##*/}
     name="${base%.xls}.csv"
     ssconvert -S "$file" "data/csv/$name"
 done
 
+### Files have two header rows, the second row is more granular so delete the first for the files we use
+
+# Ranked Measure Data Sheet
+for file in data/csv/*.csv.3; do
+    sed -i '1d' $file
+done
+
+# Additional Measure Data Sheet
 for file in data/csv/*.csv.4; do
     sed -i '1d' $file
 done
